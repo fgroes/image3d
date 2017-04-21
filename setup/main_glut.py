99b41d924540393454e19049e17b94ff693e5445
friedrich groestlinger <friedrich.groestlinger@gmail.com>
2017-04-22 01:06:04 +0200

    converted program to class

--- setup/main_glut.py
+++ setup/main_glut.py
@@ -12,214 +12,214 @@ import pyrr
 from PIL import Image
 
 
-WIDTH = 800
-HEIGHT = 600
-NAME = "My OpenGL window"
-
-
-vertices = [
-    -0.5, -0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0,
-    0.5, -0.5, 0.5, 0.0, 0.0, 0.0, 1.0, 0.0,
-    0.5, 0.5, 0.5, 0.0, 0.0, 0.0, 1.0, 1.0,
-    -0.5, 0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 1.0,
-
-    -0.5, -0.5, -0.5, 0.0, 0.0, 0.0, 0.0, 0.0,
-    0.5, -0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 0.0,
-    0.5, 0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 1.0,
-    -0.5, 0.5, -0.5, 0.0, 0.0, 0.0, 0.0, 1.0,
-
-    0.5, -0.5, -0.5, 0.0, 0.0, 0.0, 0.0, 0.0,
-    0.5, 0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 0.0,
-    0.5, 0.5, 0.5, 0.0, 0.0, 0.0, 1.0, 1.0,
-    0.5, -0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 1.0,
-
-    -0.5, 0.5, -0.5, 0.0, 0.0, 0.0, 0.0, 0.0,
-    -0.5, -0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 0.0,
-    -0.5, -0.5, 0.5, 0.0, 0.0, 0.0, 1.0, 1.0,
-    -0.5, 0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 1.0,
-
-    -0.5, -0.5, -0.5, 0.0, 0.0, 0.0, 0.0, 0.0,
-    0.5, -0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 0.0,
-    0.5, -0.5, 0.5, 0.0, 0.0, 0.0, 1.0, 1.0,
-    -0.5, -0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 1.0,
-
-    0.5, 0.5, -0.5, 0.0, 0.0, 0.0, 0.0, 0.0,
-    -0.5, 0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 0.0,
-    -0.5, 0.5, 0.5, 0.0, 0.0, 0.0, 1.0, 1.0,
-    0.5, 0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 1.0,
-]
-vertices = np.array(vertices, dtype=np.float32)
-
-
-indices = [
-    0, 1, 2, 2, 3, 0,
-    4, 5, 6, 6, 7, 4,
-    8, 9, 10, 10, 11, 8,
-    12, 13, 14, 14, 15, 12,
-    16, 17, 18, 18, 19, 16,
-    20, 21, 22, 22, 23, 20
-]
-num_indices = len(indices)
-print(num_indices)
-indices = np.array(indices, dtype=np.uint32)
-
-
-
 vertex_shader_code = \
-"""
-#version 330
+    """
+    #version 330
 
-in vec3 position;
-//in vec3 color;
-in vec2 texture_coord;
-uniform mat4 transform;
-uniform mat4 view;
-uniform mat4 model;
-uniform mat4 projection;
+    in vec3 position;
+    //in vec3 color;
+    in vec2 texture_coord;
+    uniform mat4 transform;
+    uniform mat4 view;
+    uniform mat4 model;
+    uniform mat4 projection;
 
-//out vec3 new_color;
-out vec2 new_texture_coord;
+    //out vec3 new_color;
+    out vec2 new_texture_coord;
 
-void main()
-{
-    gl_Position = projection * view * model * transform * vec4(position, 1.0f);
-    //new_color = color;
-    new_texture_coord = texture_coord;
-}
-"""
+    void main()
+    {
+        gl_Position = projection * view * model * transform * vec4(position, 1.0f);
+        //new_color = color;
+        new_texture_coord = texture_coord;
+    }
+    """
 
 
 fragment_shader_code = \
-"""
-#version 330
+    """
+    #version 330
 
-//in vec3 new_color;
-in vec2 new_texture_coord;
+    //in vec3 new_color;
+    in vec2 new_texture_coord;
 
-out vec4 out_color;
-uniform sampler2D sampler_texture;
+    out vec4 out_color;
+    uniform sampler2D sampler_texture;
 
-void main()
-{
-    out_color = texture(sampler_texture, new_texture_coord); //* new_color[0];
-}
-"""
-program = None
+    void main()
+    {
+        out_color = texture(sampler_texture, new_texture_coord); //* new_color[0];
+    }
+    """
 
+class OpenGlProgram(object):
 
-def initialize():
-    global program
+    WIDTH = 800
+    HEIGHT = 600
+    NAME = "My OpenGL window"
 
-    dim_vertex = 3
-    dim_color = 3
-    dim_texture = 2
-
-    vertex_shader = shaders.compileShader(vertex_shader_code, GL_VERTEX_SHADER)
-    fragment_shader = shaders.compileShader(fragment_shader_code, GL_FRAGMENT_SHADER)
-    program = shaders.compileProgram(vertex_shader, fragment_shader)
-
-    vao = glGenVertexArrays(1)
-    glBindVertexArray(vao)
-
-    vbo = glGenBuffers(1)
-    glBindBuffer(GL_ARRAY_BUFFER, vbo)
-    print(vertices.nbytes)
-    glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
-
-    ebo = glGenBuffers(1)
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo)
-    print(indices.nbytes)
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.nbytes, indices, GL_STATIC_DRAW)
-
-    position = glGetAttribLocation(program, "position")
-    stride = (dim_vertex + dim_color + dim_texture) * vertices.itemsize
-    offset = ctypes.c_void_p(0)
-    glVertexAttribPointer(position, dim_vertex, GL_FLOAT, GL_FALSE, stride, offset)
-    glEnableVertexAttribArray(position)
-
-    # color = glGetAttribLocation(program, "color")
-    # offset = ctypes.c_void_p(dim_vertex * vertices.itemsize)
-    # glVertexAttribPointer(color, dim_color, GL_FLOAT, GL_FALSE, stride, offset)
-    # glEnableVertexAttribArray(color)
-
-    offset = ctypes.c_void_p((dim_vertex + dim_color) * vertices.itemsize)
-    tex = glGetAttribLocation(program, "texture_coord")
-    glVertexAttribPointer(tex, dim_texture, GL_FLOAT, GL_FALSE, stride, offset)
-    glEnableVertexAttribArray(tex)
-
-    texture = glGenTextures(1)
-    glBindTexture(GL_TEXTURE_2D, texture)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-    image = Image.open("crate.jpg")
-    img_data = np.array(list(image.getdata()), np.uint8)
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.width, image.height, 0, GL_RGB, GL_UNSIGNED_BYTE, img_data)
-
-    glUseProgram(program)
+    def __init__(self):
 
 
-def main():
-    global  program
+        self.vertices = [
+            -0.5, -0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.5, -0.5, 0.5, 0.0, 0.0, 0.0, 1.0, 0.0,
+            0.5, 0.5, 0.5, 0.0, 0.0, 0.0, 1.0, 1.0,
+            -0.5, 0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 1.0,
 
-    glutInit(sys.argv)
+            -0.5, -0.5, -0.5, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.5, -0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 0.0,
+            0.5, 0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 1.0,
+            -0.5, 0.5, -0.5, 0.0, 0.0, 0.0, 0.0, 1.0,
 
-    glutInitContextVersion(3, 3)
-    glutInitContextFlags(GLUT_FORWARD_COMPATIBLE)
-    glutInitContextProfile(GLUT_CORE_PROFILE)
+            0.5, -0.5, -0.5, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.5, 0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 0.0,
+            0.5, 0.5, 0.5, 0.0, 0.0, 0.0, 1.0, 1.0,
+            0.5, -0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 1.0,
 
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
-    glutInitWindowSize(WIDTH, HEIGHT)
-    glutCreateWindow(NAME)
+            -0.5, 0.5, -0.5, 0.0, 0.0, 0.0, 0.0, 0.0,
+            -0.5, -0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 0.0,
+            -0.5, -0.5, 0.5, 0.0, 0.0, 0.0, 1.0, 1.0,
+            -0.5, 0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 1.0,
 
-    initialize()
+            -0.5, -0.5, -0.5, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.5, -0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 0.0,
+            0.5, -0.5, 0.5, 0.0, 0.0, 0.0, 1.0, 1.0,
+            -0.5, -0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 1.0,
 
-    glClearColor(0.2, 0.3, 0.2, 1.0)
-    glEnable(GL_DEPTH_TEST)
-    #glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
-
-    model = pyrr.matrix44.create_from_translation(pyrr.Vector3([0.0, 0.0, 0.0]))
-    view = pyrr.matrix44.create_from_translation(pyrr.Vector3([0.0, 0.0, -3.0]))
-    projection = pyrr.matrix44.create_perspective_projection_matrix(45.0, WIDTH / HEIGHT, 0.1, 100.0)
-
-    model_loc = glGetUniformLocation(program, "model")
-    view_loc = glGetUniformLocation(program, "view")
-    proj_loc = glGetUniformLocation(program, "projection")
-
-    glUniformMatrix4fv(model_loc, 1, GL_FALSE, model)
-    glUniformMatrix4fv(view_loc, 1, GL_FALSE, view)
-    glUniformMatrix4fv(proj_loc, 1, GL_FALSE, projection)
-
-    glutDisplayFunc(display)
-    glutTimerFunc(10, timer, 10)
-
-    glutMainLoop()
+            0.5, 0.5, -0.5, 0.0, 0.0, 0.0, 0.0, 0.0,
+            -0.5, 0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 0.0,
+            -0.5, 0.5, 0.5, 0.0, 0.0, 0.0, 1.0, 1.0,
+            0.5, 0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 1.0,
+        ]
+        self.vertices = np.array(self.vertices, dtype=np.float32)
 
 
-def timer(value):
-    time = 1.0 * glutGet(GLUT_ELAPSED_TIME) / 10000
+        self.indices = [
+            0, 1, 2, 2, 3, 0,
+            4, 5, 6, 6, 7, 4,
+            8, 9, 10, 10, 11, 8,
+            12, 13, 14, 14, 15, 12,
+            16, 17, 18, 18, 19, 16,
+            20, 21, 22, 22, 23, 20
+        ]
+        self.num_indices = len(self.indices)
+        print(self.num_indices)
+        self.indices = np.array(self.indices, dtype=np.uint32)
 
-    rot_x = pyrr.Matrix44.from_x_rotation(np.pi * time)
-    rot_y = pyrr.Matrix44.from_y_rotation(np.pi * time)
-    rot_z = pyrr.Matrix44.from_z_rotation(np.pi * time)
-
-    transform_loc = glGetUniformLocation(program, "transform")
-    glUniformMatrix4fv(transform_loc, 1, GL_FALSE, rot_x * rot_y * rot_z)
-    glutPostRedisplay()
-    glutTimerFunc(value, timer, value)
+        self.program = None
 
 
-def display():
-    global program
+    def initialize(self):
+        dim_vertex = 3
+        dim_color = 3
+        dim_texture = 2
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        vertex_shader = shaders.compileShader(vertex_shader_code, GL_VERTEX_SHADER)
+        fragment_shader = shaders.compileShader(fragment_shader_code, GL_FRAGMENT_SHADER)
+        self.program = shaders.compileProgram(vertex_shader, fragment_shader)
 
-    #glDrawArrays(GL_TRIANGLES, num_skip, num_vertices)
-    glDrawElements(GL_TRIANGLES, num_indices, GL_UNSIGNED_INT, None)
+        vao = glGenVertexArrays(1)
+        glBindVertexArray(vao)
 
-    glutSwapBuffers()
+        vbo = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, vbo)
+        print(self.vertices.nbytes)
+        glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_STATIC_DRAW)
+
+        ebo = glGenBuffers(1)
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo)
+        print(self.indices.nbytes)
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, self.indices.nbytes, self.indices, GL_STATIC_DRAW)
+
+        position = glGetAttribLocation(self.program, "position")
+        stride = (dim_vertex + dim_color + dim_texture) * self.vertices.itemsize
+        offset = ctypes.c_void_p(0)
+        glVertexAttribPointer(position, dim_vertex, GL_FLOAT, GL_FALSE, stride, offset)
+        glEnableVertexAttribArray(position)
+
+        # color = glGetAttribLocation(program, "color")
+        # offset = ctypes.c_void_p(dim_vertex * self.vertices.itemsize)
+        # glVertexAttribPointer(color, dim_color, GL_FLOAT, GL_FALSE, stride, offset)
+        # glEnableVertexAttribArray(color)
+
+        offset = ctypes.c_void_p((dim_vertex + dim_color) * self.vertices.itemsize)
+        tex = glGetAttribLocation(self.program, "texture_coord")
+        glVertexAttribPointer(tex, dim_texture, GL_FLOAT, GL_FALSE, stride, offset)
+        glEnableVertexAttribArray(tex)
+
+        texture = glGenTextures(1)
+        glBindTexture(GL_TEXTURE_2D, texture)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        image = Image.open("crate.jpg")
+        img_data = np.array(list(image.getdata()), np.uint8)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.width, image.height, 0, GL_RGB, GL_UNSIGNED_BYTE, img_data)
+
+        glUseProgram(self.program)
+
+
+    def main(self):
+
+        glutInit(sys.argv)
+
+        glutInitContextVersion(3, 3)
+        glutInitContextFlags(GLUT_FORWARD_COMPATIBLE)
+        glutInitContextProfile(GLUT_CORE_PROFILE)
+
+        glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+        glutInitWindowSize(self.WIDTH, self.HEIGHT)
+        glutCreateWindow(self.NAME)
+
+        self.initialize()
+
+        glClearColor(0.2, 0.3, 0.2, 1.0)
+        glEnable(GL_DEPTH_TEST)
+        #glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+
+        model = pyrr.matrix44.create_from_translation(pyrr.Vector3([0.0, 0.0, 0.0]))
+        view = pyrr.matrix44.create_from_translation(pyrr.Vector3([0.0, 0.0, -3.0]))
+        projection = pyrr.matrix44.create_perspective_projection_matrix(45.0, self.WIDTH / self.HEIGHT, 0.1, 100.0)
+
+        model_loc = glGetUniformLocation(self.program, "model")
+        view_loc = glGetUniformLocation(self.program, "view")
+        proj_loc = glGetUniformLocation(self.program, "projection")
+
+        glUniformMatrix4fv(model_loc, 1, GL_FALSE, model)
+        glUniformMatrix4fv(view_loc, 1, GL_FALSE, view)
+        glUniformMatrix4fv(proj_loc, 1, GL_FALSE, projection)
+
+        glutDisplayFunc(self.display)
+        glutTimerFunc(10, self.timer, 10)
+
+        glutMainLoop()
+
+
+    def timer(self, value):
+        time = 1.0 * glutGet(GLUT_ELAPSED_TIME) / 10000
+
+        rot_x = pyrr.Matrix44.from_x_rotation(np.pi * time)
+        rot_y = pyrr.Matrix44.from_y_rotation(np.pi * time)
+        rot_z = pyrr.Matrix44.from_z_rotation(np.pi * time)
+
+        transform_loc = glGetUniformLocation(self.program, "transform")
+        glUniformMatrix4fv(transform_loc, 1, GL_FALSE, rot_x * rot_y * rot_z)
+        glutPostRedisplay()
+        glutTimerFunc(value, self.timer, value)
+
+
+    def display(self):
+        global program
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        glDrawElements(GL_TRIANGLES, self.num_indices, GL_UNSIGNED_INT, None)
+
+        glutSwapBuffers()
 
 
 if __name__ == "__main__":
-    main()
+    program = OpenGlProgram()
+    program.main()

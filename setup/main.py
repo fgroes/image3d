8761d81ae1e3ec0e533bd586dce8e321d3987cc4
friedrich groestlinger <friedrich.groestlinger@gmail.com>
2017-04-22 17:25:10 +0200

    refactor shader programs and loading into separate files

--- setup/main.py
+++ setup/main.py
@@ -10,47 +10,8 @@ import numpy as np
 import ctypes
 import pyrr
 from PIL import Image
+from shader import ShaderProgram
 
-
-vertex_shader_code = \
-    """
-    #version 330
-
-    in vec3 position;
-    //in vec3 color;
-    in vec2 texture_coord;
-    uniform mat4 transform;
-    uniform mat4 view;
-    uniform mat4 model;
-    uniform mat4 projection;
-
-    //out vec3 new_color;
-    out vec2 new_texture_coord;
-
-    void main()
-    {
-        gl_Position = projection * view * model * transform * vec4(position, 1.0f);
-        //new_color = color;
-        new_texture_coord = texture_coord;
-    }
-    """
-
-
-fragment_shader_code = \
-    """
-    #version 330
-
-    //in vec3 new_color;
-    in vec2 new_texture_coord;
-
-    out vec4 out_color;
-    uniform sampler2D sampler_texture;
-
-    void main()
-    {
-        out_color = texture(sampler_texture, new_texture_coord); //* new_color[0];
-    }
-    """
 
 class OpenGlProgram(object):
 
@@ -58,20 +19,17 @@ class OpenGlProgram(object):
     HEIGHT = 600
     NAME = "My OpenGL window"
 
-    def __init__(self, vertices, indices):
+    def __init__(self, vertices, indices, shader_program):
         self.vertices = vertices
         self.indices = indices
-        self.program = None
-
+        self.shader_program = shader_program
 
     def initialize(self):
         dim_vertex = 3
         dim_color = 3
         dim_texture = 2
 
-        vertex_shader = shaders.compileShader(vertex_shader_code, GL_VERTEX_SHADER)
-        fragment_shader = shaders.compileShader(fragment_shader_code, GL_FRAGMENT_SHADER)
-        self.program = shaders.compileProgram(vertex_shader, fragment_shader)
+        self.shader_program.compile()
 
         vao = glGenVertexArrays(1)
         glBindVertexArray(vao)
@@ -84,7 +42,7 @@ class OpenGlProgram(object):
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo)
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, self.indices.nbytes, self.indices, GL_STATIC_DRAW)
 
-        position = glGetAttribLocation(self.program, "position")
+        position = glGetAttribLocation(self.shader_program.program, "position")
         stride = (dim_vertex + dim_color + dim_texture) * self.vertices.itemsize
         offset = ctypes.c_void_p(0)
         glVertexAttribPointer(position, dim_vertex, GL_FLOAT, GL_FALSE, stride, offset)
@@ -96,7 +54,7 @@ class OpenGlProgram(object):
         # glEnableVertexAttribArray(color)
 
         offset = ctypes.c_void_p((dim_vertex + dim_color) * self.vertices.itemsize)
-        tex = glGetAttribLocation(self.program, "texture_coord")
+        tex = glGetAttribLocation(self.shader_program.program, "texture_coord")
         glVertexAttribPointer(tex, dim_texture, GL_FLOAT, GL_FALSE, stride, offset)
         glEnableVertexAttribArray(tex)
 
@@ -110,7 +68,7 @@ class OpenGlProgram(object):
         img_data = np.array(list(image.getdata()), np.uint8)
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.width, image.height, 0, GL_RGB, GL_UNSIGNED_BYTE, img_data)
 
-        glUseProgram(self.program)
+        glUseProgram(self.shader_program.program)
 
 
     def main(self):
@@ -135,9 +93,9 @@ class OpenGlProgram(object):
         view = pyrr.matrix44.create_from_translation(pyrr.Vector3([0.0, 0.0, -3.0]))
         projection = pyrr.matrix44.create_perspective_projection_matrix(45.0, self.WIDTH / self.HEIGHT, 0.1, 100.0)
 
-        model_loc = glGetUniformLocation(self.program, "model")
-        view_loc = glGetUniformLocation(self.program, "view")
-        proj_loc = glGetUniformLocation(self.program, "projection")
+        model_loc = glGetUniformLocation(self.shader_program.program, "model")
+        view_loc = glGetUniformLocation(self.shader_program.program, "view")
+        proj_loc = glGetUniformLocation(self.shader_program.program, "projection")
 
         glUniformMatrix4fv(model_loc, 1, GL_FALSE, model)
         glUniformMatrix4fv(view_loc, 1, GL_FALSE, view)
@@ -156,7 +114,7 @@ class OpenGlProgram(object):
         rot_y = pyrr.Matrix44.from_y_rotation(np.pi * time)
         rot_z = pyrr.Matrix44.from_z_rotation(np.pi * time)
 
-        transform_loc = glGetUniformLocation(self.program, "transform")
+        transform_loc = glGetUniformLocation(self.shader_program.program, "transform")
         glUniformMatrix4fv(transform_loc, 1, GL_FALSE, rot_x * rot_y * rot_z)
         glutPostRedisplay()
         glutTimerFunc(value, self.timer, value)
@@ -170,5 +128,8 @@ class OpenGlProgram(object):
 
 if __name__ == "__main__":
     from crate_data import vertices, indices
-    program = OpenGlProgram(vertices, indices)
+    vertex_shader_filename = "vertex_shader.sdr"
+    fragment_shader_filename = "fragment_shader.sdr"
+    shader_program = ShaderProgram(vertex_shader_filename, fragment_shader_filename)
+    program = OpenGlProgram(vertices, indices, shader_program)
     program.main()
